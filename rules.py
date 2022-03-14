af5719be1f30bb35b5c6498d3dddb0e8bf5877a8
Tordbidos7 <danybuschi@gmail.com>
2022-03-14 15:52:30 +0100

    Rimesso a posto ciò che avevo distrutto

--- rules.py
+++ rules.py
@@ -51,7 +51,7 @@ def generate_state(sym1, sym2):
     string_list[random.randint(0,9)]= sym2     #insert the 0 at a random position using a convertion to a list
     return string_list
 
-def evolve(stato):
+def evolve(stato, rule_number):
     new_stato = ['']*12
     new_stato[0] = '.'
     new_stato[11] = '.'
@@ -60,21 +60,21 @@ def evolve(stato):
         seq = (stato[i-1], stato[i], stato[i+1])
         terna = ''
         terna = ''.join(seq)
-        new_stato[i] = rule30[terna]
+        new_stato[i] = rules[rule_number][terna]
     print(''.join(new_stato))
     return ''.join(new_stato)
 
-def simulation(nsteps):
-    initial_state = generate_state(".", "0")
+def simulation(nsteps, rule_number):
+    initial_state = generate_state()
     print(initial_state)
     states_seq = [initial_state]
     for i in range(nsteps):
         old_state = states_seq[-1]
-        new_state = evolve(old_state)
+        new_state = evolve(old_state, rule_number)
         states_seq.append(new_state)
     return states_seq
 
-simulation(10)
+simulation(10, rule_number=30)
 
 ########################################################
 def test_generation_valid_state():
