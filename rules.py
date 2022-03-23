5f9b416419f4cf7b893ff1bb1e84071ca3937c0b
Lorenzo Barsotti <lorenzo.barsotti@hotmail.com>
2022-03-23 22:44:34 +0100

    definition of some funtion in order to perform the tests

--- rules.py
+++ rules.py
@@ -72,9 +72,28 @@ def simulation(nsteps, rule_number):
         states_seq.append(new_state)
     return states_seq
 
+
 simulation(10, rule_number=30)
 
 ########################################################
+
+def is_valid_state(state):
+    """this function returns true if the given state is of formed by only . and 0, false otherwise
+    
+    GIVEN: a state
+    WHEN: I apply it to generate an initial state
+    THEN: the result is true if it contains only . and 0
+    """
+    return set(state) == {".","0"}
+
+def is_length_valid(state):
+    """this function returns true if the given state is of the correct length, false otherwise
+    
+    GIVEN: a state
+    WHEN: I apply it to generate an initial state
+    THEN: the result is true if the length is 12 
+    """
+    return len(state) == 12 
 
 def test_generation_valid_state():
     """ this tests that the generation function returns valid states when used
@@ -84,7 +103,7 @@ def test_generation_valid_state():
     THEN: the resulting state is still a valid one
     """
     state = generate_state()
-    assert set(state) == {".","0"}
+    assert is_valid_state(state)
 
 def test_generation_single_alive():
     """ this tests that the generation function returns only one alive when used
@@ -96,18 +115,7 @@ def test_generation_single_alive():
     state = generate_state()
     num_of_0 = sum(1 for i in state if i=='0')
     assert num_of_0 == 1
-
-def is_valid_state(state):
-    """this tests that the a general state is valid
-    
-    GIVEN: a state
-    WHEN: I apply it to generate an initial state
-    THEN: the resulting state has only one alive
-    """
-    print(state)
-    print(len(state)==12)
-    assert len(state)==12 #non so se può essere buona come validità
-
+ 
 def test_evolve_valid():
     """ this tests that the evolve function returns valid states when used
     
@@ -122,10 +130,4 @@ def test_evolve_valid():
 def test_length_evolved_state():
     state = generate_state()
     new_state = evolve(state, rule_number=30)
-    assert len(state) == len(new_state)
-
-def test_validness_evolved_state():
-    state = generate_state()
-    new_state = evolve(state, rule_number=30)
-    assert set(state) == {'.', '0'}
-
+    assert is_length_valid(new_state)
